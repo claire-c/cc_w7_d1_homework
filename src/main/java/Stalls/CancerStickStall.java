bf4ef8e7652c44cd49a411cef434d417abaf5687
Claire <cconnachan@gmail.com>
2018-04-16 21:18:10 +0100

    added smokingStall class, passed tests

--- src/main/java/Stalls/CancerStickStall.java
+++ src/main/java/Stalls/CancerStickStall.java
@@ -1,4 +1,28 @@
 package Stalls;
 
-public class CancerStickStall {
+import Interfaces.IRestrictable;
+import Visitors.Visitor;
+
+public class CancerStickStall extends Stall implements IRestrictable {
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+    private int funRating;
+    private double price;
+
+    public CancerStickStall(String name, String ownerName, int parkingSpot, int funRating, double price){
+        super(name, ownerName, parkingSpot, funRating, price);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if(visitor.getAge() < 18){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+
 }
--- src/test/java/StallsTests/CancerStickStallTest.java
+++ src/test/java/StallsTests/CancerStickStallTest.java
@@ -1,4 +1,43 @@
 package StallsTests;
 
+import Stalls.CancerStickStall;
+import Visitors.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class CancerStickStallTest {
+
+    private CancerStickStall smokingStall;
+    private Visitor youngVisitor;
+    private Visitor oldVisitor;
+
+    @Before
+    public void before(){
+        smokingStall = new CancerStickStall("smoking", "Phillis", 3, 0, 10.0);
+        youngVisitor = new Visitor(10, 100, 10.0);
+        oldVisitor = new Visitor(20, 200, 20.0);
+    }
+
+    @Test
+    public void notAllowedVisitorTooYoung(){
+        assertFalse(smokingStall.isAllowedTo(youngVisitor));
+    }
+
+    @Test
+    public void allowedToBuyVisitorOldEnough(){
+        assertTrue(smokingStall.isAllowedTo(oldVisitor));
+    }
+
+    @Test
+    public void canGetPrice(){
+        assertEquals(10.0, smokingStall.priceFor(oldVisitor), 0.1);
+    }
+
+
+
+
 }
