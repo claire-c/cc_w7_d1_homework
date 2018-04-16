3465ca52fe9c3b714a7a715c2ebda67a92bd3ca3
Claire <cconnachan@gmail.com>
2018-04-16 20:31:39 +0100

    added rollercoaster class, passed first tests

--- src/main/java/Attractions/Rollercoaster.java
+++ src/main/java/Attractions/Rollercoaster.java
@@ -1,4 +1,42 @@
 package Attractions;
 
-public class Rollercoaster {
+import Interfaces.IChargeable;
+import Interfaces.IRestrictable;
+import Visitors.Visitor;
+
+public class Rollercoaster extends Attraction implements IChargeable, IRestrictable{
+
+    private String name;
+    private int funRating;
+    private double price;
+
+    public Rollercoaster(String name, int funRating, double price){
+        super(name, funRating);
+        this.price = price;
+    }
+
+    public double priceFor(Visitor visitor){
+        double cost;
+        if(visitor.getAge() < 12){
+            cost = (price/2);
+        }else{
+            cost = price;
+        }
+        return cost;
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if(visitor.getAge() <= 12 && visitor.getHeight() <= 145){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+
+
+
+
+
 }
--- src/test/java/AttractionsTests/RollercoasterTest.java
+++ src/test/java/AttractionsTests/RollercoasterTest.java
@@ -1,4 +1,57 @@
 package AttractionsTests;
 
+import Attractions.Rollercoaster;
+import Visitors.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RollercoasterTest {
+
+    private Rollercoaster rollercoaster;
+    private Visitor shortVisitor;
+    private Visitor tallVisitor;
+    private Visitor oldVisitor;
+    private Visitor youngVisitor;
+    private Visitor youngVisitor2;
+
+    @Before
+    public void before(){
+        rollercoaster = new Rollercoaster("rollercoaster", 9, 7.0);
+        shortVisitor = new Visitor(20, 140, 10.0);
+        tallVisitor = new Visitor(21, 150, 20.0);
+        oldVisitor = new Visitor(40, 160, 40.0);
+        youngVisitor = new Visitor(10, 150, 10.0);
+        youngVisitor2 = new Visitor(12, 130, 20.0);
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("rollercoaster", rollercoaster.getName());
+    }
+
+    @Test
+    public void canGetPriceForVisitor(){
+        assertEquals(7.0, rollercoaster.priceFor(tallVisitor), 0.1);
+    }
+
+    @Test
+    public void canGetPriceForYoungVisitor(){
+        assertEquals(3.5, rollercoaster.priceFor(youngVisitor), 0.1);
+    }
+
+//    @Test
+//    public void cannotGetPriceForVisitorTooYoung(){
+//
+//    }
+//
+//    @Test
+//    public void cannotGetPriceForVisitorTooShort(){
+//
+//    }
+
+
+
 }
