14403d997acd999227ebfcbf2fd6b59c48c10d0a
Claire <cconnachan@gmail.com>
2018-04-16 17:59:48 +0100

    added playground class, passed tests

--- src/main/java/Attractions/Playground.java
+++ src/main/java/Attractions/Playground.java
@@ -1,4 +1,23 @@
 package Attractions;
 
-public class Playground {
+import Interfaces.IRestrictable;
+import Visitors.Visitor;
+
+public class Playground extends Attraction implements IRestrictable{
+
+    private String name;
+    private int funRating;
+
+    public Playground(String name, int funRating){
+        super(name, funRating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if(visitor.getAge() < 15){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
--- src/test/java/AttractionsTests/PlaygroundTest.java
+++ src/test/java/AttractionsTests/PlaygroundTest.java
@@ -1,4 +1,47 @@
 package AttractionsTests;
 
+import Attractions.Playground;
+import Visitors.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class PlaygroundTest {
+
+    private Playground playground;
+    private Visitor youngVisitor;
+    private Visitor oldVisitor;
+
+    @Before
+    public void before(){
+        playground = new Playground("playground", 6);
+        youngVisitor = new Visitor(10, 100, 10.0);
+        oldVisitor = new Visitor(20, 170, 50.0);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("playground", playground.getName());
+    }
+
+    @Test
+    public void canGetFunRating(){
+        assertEquals(6, playground.getFunRating());
+    }
+
+    @Test
+    public void visitorCanAccessPlayground(){
+        assertTrue(playground.isAllowedTo(youngVisitor));
+    }
+
+    @Test
+    public void visitorCannotAccessPlayground(){
+        assertFalse(playground
+        .isAllowedTo(oldVisitor));
+    }
+
+
 }
