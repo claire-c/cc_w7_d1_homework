b563db9056d812563299be58dd8f4248c440e511
Claire <cconnachan@gmail.com>
2018-04-16 17:31:22 +0100

    added abstract class - attraction

---
+++ src/main/java/Attractions/Attraction.java
@@ -0,0 +1,16 @@
+package Attractions;
+
+import Interfaces.IEnjoyable;
+
+public abstract class Attraction implements IEnjoyable {
+
+    private String name;
+    private int funRating;
+
+    public Attraction(String name, int funRating) {
+        this.name = name;
+        this.funRating = funRating;
+    }
+
+
+}
