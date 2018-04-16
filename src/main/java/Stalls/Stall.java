83eaa9892d0c06f24a18dd08e82fcf8a60fdee26
Claire <cconnachan@gmail.com>
2018-04-16 17:39:06 +0100

    added abstract stall class

--- src/main/java/Stalls/Stall.java
+++ src/main/java/Stalls/Stall.java
@@ -1,4 +1,39 @@
 package Stalls;
 
-public class Stall {
+import Interfaces.IChargeable;
+import Interfaces.IEnjoyable;
+
+public abstract class Stall implements IEnjoyable, IChargeable {
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+    private int funRating;
+
+    public Stall(String name, String ownerName, int parkingSpot, int funRating){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+        this.funRating = funRating;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getOwnerName(){
+        return this.ownerName;
+    }
+
+    public int getParkingSpot(){
+        return this.parkingSpot;
+    }
+
+    public int getFunRating(){
+        return this.funRating;
+    }
+
+    
+
+
 }
