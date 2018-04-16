1f5bf78c9a571e2cd23db988fd7a52721ef49f70
Claire <cconnachan@gmail.com>
2018-04-16 18:14:59 +0100

    added dogdems class, passed tests

--- src/main/java/Attractions/Dodgems.java
+++ src/main/java/Attractions/Dodgems.java
@@ -1,4 +1,31 @@
 package Attractions;
 
-public class Dodgems {
+import Interfaces.IChargeable;
+import Visitors.Visitor;
+
+public class Dodgems extends Attraction implements IChargeable{
+
+    private String name;
+    private int funRating;
+    private double price;
+
+    public Dodgems(String name, int funRating, double price){
+        super(name, funRating);
+        this.price = price;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public double priceFor(Visitor visitor){
+         double cost;
+        if(visitor.getAge() < 12){
+            cost = (price/2);
+        }else{
+            cost = price;
+        }
+        return cost;
+    }
+
 }
--- src/test/java/AttractionsTests/DodgemsTest.java
+++ src/test/java/AttractionsTests/DodgemsTest.java
@@ -1,4 +1,39 @@
 package AttractionsTests;
 
+import Attractions.Dodgems;
+import Visitors.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    private Dodgems dodgems;
+    private Visitor youngVisitor;
+    private Visitor oldVisitor;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("dodgems", 7, 5.0);
+        youngVisitor = new Visitor(10, 100, 10.0);
+        oldVisitor = new Visitor(20, 170, 10.0);
+    }
+
+    @Test
+    public void canGetPrice(){
+        assertEquals(5.0, dodgems.getPrice(), 0.1);
+    }
+
+    @Test
+    public void willReturnHalfPriceTicket(){
+        assertEquals(2.5, dodgems.priceFor(youngVisitor), 0.1);
+    }
+
+    @Test
+    public void willReturnFullPriceTicket(){
+        assertEquals(5.0, dodgems.priceFor(oldVisitor), 0.1);
+    }
+
+
 }
