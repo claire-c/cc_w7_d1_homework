6d051545462214d19ae83424679ac74f173fa9a7
Claire <cconnachan@gmail.com>
2018-04-16 21:09:01 +0100

    added CandyFlossStall class, passed tests

--- src/main/java/Stalls/CandyFlossStall.java
+++ src/main/java/Stalls/CandyFlossStall.java
@@ -1,4 +1,20 @@
 package Stalls;
 
-public class CandyFlossStall {
+public class CandyFlossStall extends Stall{
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+    private int funRating;
+    private double price;
+
+    public CandyFlossStall(String name, String ownerName, int parkingSpot, int funRating, double price){
+        super(name, ownerName, parkingSpot, funRating, price);
+    }
+
+
+
+
+
 }
+
--- src/test/java/StallsTests/CandyFlossStallTest.java
+++ src/test/java/StallsTests/CandyFlossStallTest.java
@@ -1,4 +1,32 @@
 package StallsTests;
 
+import Stalls.CandyFlossStall;
+import Visitors.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CandyFlossStallTest {
+
+    private CandyFlossStall candyFlossStall;
+    private Visitor visitor;
+
+    @Before
+    public void before(){
+        candyFlossStall = new CandyFlossStall("candy", "Claire", 2, 2, 4.0);
+        visitor = new Visitor(8, 75, 5.0);
+    }
+
+    @Test
+    public void canGetPrice(){
+        assertEquals(4.0, candyFlossStall.priceFor(visitor), 0.1);
+    }
+
+    @Test
+    public void canGetFunRating(){
+        assertEquals(2, candyFlossStall.getFunRating());
+    }
+
+
 }
